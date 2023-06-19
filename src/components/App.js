fa7a49b193051a83b7ee62b0df48c28188fe53fb
Derek Truesdale <dtruesdale37@gmail.com>
2023-06-19 15:27:01 -0500

    up to part3

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [taskdata, setData]= useState(TASKS)
+  const [categorydata, setCategoryData]=useState(CATEGORIES)
+
+  function deleteTask(taskObj){
+    console.log('taskObj:',taskObj)
+    const filteredObj=taskdata.filter(object=> object.text!==taskObj)
+    console.log('filteredObject:',filteredObj)
+    setData(filteredObj)
+  }
+
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter categories={categorydata} />
       <NewTaskForm />
-      <TaskList />
+      <TaskList  tasks={taskdata} deleteTask={deleteTask}/>
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories}) {
+  const [buttonClass, setButtonClass]=useState(true)
+  
+  const displayCategoriesButtons=categories.map((category)=>{
+    return(<button 
+      key={category}  
+      onClick={selectedButton} 
+      className={buttonClass}>{category}
+      </button>)
+  })
+
+  function selectedButton(event){
+    console.log(event.target)
+    const filteredButton=categories.filter(categoryButton=>{
+      return categoryButton===event.target})
+    console.log(filteredButton)
+    // setButtonClass((buttonClass)=> !buttonClass)
+    return filteredButton.setButtonClass((buttonClass)=> !buttonClass)
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {displayCategoriesButtons}
     </div>
   );
 }
